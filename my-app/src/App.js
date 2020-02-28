b31e648668851888266976b2e9abe09db66852b9
david-develop <924@holbertonschool.com>
2020-02-28 15:01:18 -0500

    [Update] Button, Search and Table to stateless

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -63,36 +63,61 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <div>
-            <form>
-              <input 
-                type="text" 
-                value={searchTerm}
-                onChange={this.onSearchChange}
-              />
-            </form>
-            { list.filter(isSearched(searchTerm)).map(item =>
-            <div key={item.objectID}>
-              <h2>
-                <a href={item.url}> {item.title} </a>
-              </h2>
-              <span> Author: {item.author} </span>
-              <span> Comments: {item.num_comments} </span>
-              <span> Points: {item.points} </span>
-              <span>
-                <button
-                  onClick={() => this.onDismiss(item.objectID)}
-                  type="button"
-                >
-                  Dismiss
-                </button>
-              </span>
-              <br /><br />
-            </div>
-            )}
+            <Search
+              value={searchTerm}
+              onChange={this.onSearchChange}
+            >
+              Search
+            </Search>
+            <br />
+            <Table
+              list={list}
+              pattern={searchTerm}
+              onDismiss={this.onDismiss}
+            />
           </div>
         </header>
       </div>
     );
   }
 }
+
+const Search = ({ value, onChange, children }) =>
+  <form>
+    {children} <input
+      type="text"
+      value={value}
+      onChange={onChange}
+    />
+  </form>
+
+const Table = ({ list, pattern, onDismiss }) =>
+  <div>
+    {list.filter(isSearched(pattern)).map(item =>
+      <div key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span> Author: {item.author} </span>
+        <span> Comments: {item.num_comments} </span>
+        <span> Points: {item.points} </span>
+        <span>
+          <Button onClick={() => onDismiss(item.objectID)}>
+            Dismiss
+          </Button>
+          <br /><br />
+        </span>
+      </div>
+    )}
+  </div>
+
+const Button = ({onClick, className = ' ', children}) =>
+      <button
+        onClick={onClick}
+        className={className}
+        type="button"
+      >
+        {children}
+      </button>
+
 export default App;
